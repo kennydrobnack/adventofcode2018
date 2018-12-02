4b42c22c88a29253a575c09055d852caeeaae0f0
Kenny Drobnack <kennydrobnack@gmail.com>
2018-12-02 09:39:48 -0500

    Got the basic checksumming working, I think

--- day02/checksumboxids/src/main/scala/ChecksumBoxes.scala
+++ day02/checksumboxids/src/main/scala/ChecksumBoxes.scala
@@ -1,5 +1,11 @@
 object ChecksumBoxes extends App {
-  def checksum(boxLabels: List[String]) : Int = {
-    0
+  def calculateChecksum(boxLabels: List[String]): Int = {
+    val boxesWithTwoLettersRepeating = boxLabels.filter(b => letterOccursNumberOfTimesOnLabel(b, 2)).length
+    val boxesWithThreeLettersRepeating = boxLabels.filter(b => letterOccursNumberOfTimesOnLabel(b, 3)).length
+    boxesWithTwoLettersRepeating * boxesWithThreeLettersRepeating 
+  }
+
+  def letterOccursNumberOfTimesOnLabel(boxLabel: String, times: Int): Boolean ={
+    boxLabel.groupBy(c => c.toLower).map(e => (e._2.length, e._1)).contains(times)
   }
 }
--- day02/checksumboxids/src/test/scala/ChecksumBoxesTest.scala
+++ day02/checksumboxids/src/test/scala/ChecksumBoxesTest.scala
@@ -1,5 +1,16 @@
 class ChecksumBoxesTest extends org.scalatest.FunSuite {
-  test("ChecksumBoxes.checksum") {
-    assert(ChecksumBoxes.checksum(List()) === 0)
+  test("letterOccursNumberOfTimesOnLabel correctly returns false if letter does not occur N times") {
+    assert(ChecksumBoxes.letterOccursNumberOfTimesOnLabel("a", 2) === false)
+  }
+
+  test("letterOccursNumberOfTimesOnLabel correctly returns true if letter does occur N times") {
+    assert(ChecksumBoxes.letterOccursNumberOfTimesOnLabel("aabcdeef", 2), true)
+  }
+
+  test("ChecksumBoxes.calculateChecksum gets correct checksum with empty list") {
+    assert(ChecksumBoxes.calculateChecksum(List()) === 0)
+  }
+  test("ChecksumBoxes.calculateChecksum gets correct checksum with one item") {
+    assert(ChecksumBoxes.calculateChecksum(List("aabbbccddde")) === 1)
   }
 }
