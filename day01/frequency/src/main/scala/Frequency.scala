8219d70d9c455988563199d8226b2a4d1c63e60a
Kenny Drobnack <kennydrobnack@gmail.com>
2018-12-01 23:35:46 -0500

    Glommed together read numbers from file and calculate functions

--- day01/frequency/src/main/scala/Frequency.scala
+++ day01/frequency/src/main/scala/Frequency.scala
@@ -1,11 +1,16 @@
 import scala.io.Source
 
 object Frequency extends App {
+  val filename = args(0)
+  println("Calculating frequence drift from file: " + filename)
+  println(calc(readNumbersFromFile(filename)))
+
   def calc (numbers : List[Int]): Int = {
     numbers.sum
   }
+  
   def readNumbersFromFile(fileName: String): List[Int] = {
-      val lines = Source.fromFile(fileName).getLines.toList
-      lines.map(_.toString.toInt)
+    val lines = Source.fromFile(fileName).getLines.toList
+    lines.map(_.toString.toInt)
   }
 }
