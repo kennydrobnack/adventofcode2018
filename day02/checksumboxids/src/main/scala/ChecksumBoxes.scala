b76c37b467789789c68261a4e6309a764018a8c1
Kenny Drobnack <kennydrobnack@gmail.com>
2018-12-02 13:30:44 -0500

    Wired up file io and calculate checksum pieces for complete solution

--- day02/checksumboxids/src/main/scala/ChecksumBoxes.scala
+++ day02/checksumboxids/src/main/scala/ChecksumBoxes.scala
@@ -1,6 +1,11 @@
 import scala.io.Source
 
 object ChecksumBoxes extends App {
+  val filename = args(0)
+  println("Calculating box label checksum from file: " + filename)
+  val labels = readLablesFromFile(filename)
+  println(calculateChecksum(labels))
+
   def calculateChecksum(boxLabels: List[String]): Int = {
     val boxesWithTwoLettersRepeating = boxLabels.filter(b => letterOccursNumberOfTimesOnLabel(b, 2)).length
     val boxesWithThreeLettersRepeating = boxLabels.filter(b => letterOccursNumberOfTimesOnLabel(b, 3)).length
