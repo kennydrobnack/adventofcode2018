bdb6382e7138e36122f2cc1c4a026a9a0618b706
Kenny Drobnack <kennydrobnack@gmail.com>
2018-12-02 00:42:00 -0500

    Day 1 part 2 - finding dups is working for one loop around the list

--- day01/frequency/src/main/scala/Frequency.scala
+++ day01/frequency/src/main/scala/Frequency.scala
@@ -2,11 +2,33 @@ import scala.io.Source
 
 object Frequency extends App {
   val filename = args(0)
-  println("Calculating frequence drift from file: " + filename)
-  println(calc(readNumbersFromFile(filename)))
+  println("Calculating final frequency from file: " + filename)
+  val numbers = readNumbersFromFile(filename)
+  println(calcFinalFrequency(numbers))
+  println("Calculating first frequency seen twice: ")
+  println(findDuplicate(numbers))
 
-  def calc (numbers : List[Int]): Int = {
+  def calcFinalFrequency (numbers : List[Int]): Int = {
     numbers.sum
+  }
+
+  def findDuplicate(numbers : List[Int]): Int = {
+    var currentValue = 0
+    var seenNumbers = Map[Int, Int]()
+    seenNumbers = seenNumbers + (0 -> 1)
+    var latestSeen = 0
+    while (seenNumbers.get(latestSeen) != 2) {
+      for (number <- numbers) {
+        latestSeen = latestSeen + number
+        if (seenNumbers.contains(latestSeen)) {
+          return latestSeen 
+        }
+        else {
+          seenNumbers += (latestSeen -> 1)
+        }
+      }
+    }
+    latestSeen
   }
   
   def readNumbersFromFile(fileName: String): List[Int] = {
--- day01/frequency/src/test/scala/FrequencyTest.scala
+++ day01/frequency/src/test/scala/FrequencyTest.scala
@@ -1,17 +1,23 @@
 class FrequencyTest extends org.scalatest.FunSuite {
-  test("Frequency.calc returns 0 with empty list") {
-    assert(Frequency.calc(List()) === 0)
+  test("Frequency.findDuplicate returns first dup when it only flips away from zero and back") {
+    assert(Frequency.findDuplicate(List(1, -1)) == 0)
   }
-  test("Frequency.calc returns the number with a single number list") {
-    assert(Frequency.calc(List(42)) === 42)
+  test("Frequency.findDuplicate returns first dup when going through the list once") {
+    assert(Frequency.findDuplicate(List(2, -4, 4)) == 2)
   }
-  test("Frequency.calc returns single negative number if we have a negative number") {
-    assert(Frequency.calc(List(-42)) === -42)
+  test("Frequency.calcFinalFrequency returns 0 with empty list") {
+    assert(Frequency.calcFinalFrequency(List()) === 0)
   }
-  test("Frequency.calc returns the numbers summed up") {
-    assert(Frequency.calc(List(0, 1, 2, 3, -6, -42)) === -42)
+  test("Frequency.calcFinalFrequency returns the number with a single number list") {
+    assert(Frequency.calcFinalFrequency(List(42)) === 42)
   }
-  test("Frequency.calc can read a file and turn a list of numbers one per line into a list of Ints") {
+  test("Frequency.calcFinalFrequency returns single negative number if we have a negative number") {
+    assert(Frequency.calcFinalFrequency(List(-42)) === -42)
+  }
+  test("Frequency.calcFinalFrequency returns the numbers summed up") {
+    assert(Frequency.calcFinalFrequency(List(0, 1, 2, 3, -6, -42)) === -42)
+  }
+  test("Frequency.calcFinalFrequency can read a file and turn a list of numbers one per line into a list of Ints") {
       assert(Frequency.readNumbersFromFile("test.txt") === List(0, 1, 2, 3, -42))
   }
 }
