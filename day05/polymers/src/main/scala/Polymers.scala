40f8db052efd39feb20fd000604e451f03788b1a
Kenny Drobnack <kennydrobnack@gmail.com>
2018-12-06 22:18:33 -0500

    Got the basic algorithm to condense polymers in day05 part 1 working

--- day05/polymers/src/main/scala/Polymers.scala
+++ day05/polymers/src/main/scala/Polymers.scala
@@ -1,5 +1,28 @@
 object Polymers extends App {
-  def condense(polymers: String) = {
-    polymers
+  def condense(polymers: String) : String = {
+    var currentString = polymers
+    var droppedCharacters = true
+    while (droppedCharacters == true) {
+      val updatedString = dropMatchingCharacters(currentString)
+      if (updatedString != currentString) {
+        droppedCharacters = true
+        currentString = updatedString
+      }
+      else {
+        droppedCharacters = false
+      }
+    }
+    currentString
+  }
+
+  def dropMatchingCharacters(updateString: String): String = {
+    var charArray = updateString.toArray
+    var i = 0
+    for (i <- 0 to charArray.length - 2) {
+      if (charArray.charAt(i) != charArray.charAt(i + 1) && charArray.charAt(i).toUpper == charArray.charAt(i + 1).toUpper) {
+        return charArray.patch(i, Nil, 2).mkString
+      }
+    }
+    charArray.mkString
   }
 }
--- day05/polymers/src/test/scala/PolymersTest.scala
+++ day05/polymers/src/test/scala/PolymersTest.scala
@@ -1,5 +1,9 @@
 class PolymersTest extends org.scalatest.FunSuite {
-  test("Polymers.condense") {
+  test("Polymers.condense returns given string if no further condensation can be done") {
     assert(Polymers.condense("dabCBAcaDA") === "dabCBAcaDA")
+  }
+
+  test("Polymer.condense removes any adjacent matching upper & lowercase letters") {
+    assert(Polymers.condense("dabAcCaCBAcCcaDA") === "dabCBAcaDA")
   }
 }
