dc9c571a58a10b7c36803a3c98e5dcca6644739d
Kenny Drobnack <kennydrobnack@gmail.com>
2018-12-10 22:26:33 -0500

    Parsing claims of how fabric should be cut with regular expressions. Now I have two problems. At least one of them has a tested solution

--- day03/overlap/src/main/scala/Overlap.scala
+++ day03/overlap/src/main/scala/Overlap.scala
@@ -4,4 +4,10 @@ object Overlap extends App {
   def readFromFile(fileName: String): List[String] = {
     Source.fromFile(fileName).getLines.toList
   }
+
+  def parseClaim(claimString: String) : Map[String, List[Int]] = {
+    val pattern = "#([0-9]+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)".r
+    val pattern(claimId, margin_left, margin_top, width, height) = claimString
+    Map("claimId" -> List(claimId.toInt), "margins" -> List(margin_left.toInt, margin_top.toInt), "size" ->  List(width.toInt, height.toInt))
+  }
 }
--- day03/overlap/src/test/scala/OverlapTest.scala
+++ day03/overlap/src/test/scala/OverlapTest.scala
@@ -2,4 +2,8 @@ class OverlapTest extends org.scalatest.FunSuite {
   test("Overlap.readFromFile reads in claims from a file") {
     assert(Overlap.readFromFile("test.txt") === List("#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"))
   }
+
+  test("Parse claim gets claim id, margin and size") {
+    assert(Overlap.parseClaim("#1 @ 1,3: 4x4") === Map("claimId" -> List(1), "margins" -> List(1, 3), "size" -> List(4, 4)))
+  }
 }
