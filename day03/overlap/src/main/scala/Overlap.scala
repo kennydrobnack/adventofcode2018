d9735e04811a690f3a365f1807581cc68a386b98
Kenny Drobnack <kennydrobnack@gmail.com>
2018-12-10 23:25:35 -0500

    Figured out how to calculate which fabric squares to cut for Day 03 part 1

--- day03/overlap/src/main/scala/Overlap.scala
+++ day03/overlap/src/main/scala/Overlap.scala
@@ -10,4 +10,21 @@ object Overlap extends App {
     val pattern(claimId, margin_left, margin_top, width, height) = claimString
     Map("claimId" -> List(claimId.toInt), "margins" -> List(margin_left.toInt, margin_top.toInt), "size" ->  List(width.toInt, height.toInt))
   }
+
+  def cutFabric(claimString: String): Map[List[Int], String] = {
+    val claim = parseClaim(claimString)
+    val firstRow = claim("margins").last + 1
+    val lastRow = claim("size").last + firstRow - 1
+    val firstColumn = claim("margins").head + 1
+    val lastColumn = firstColumn + claim("size").head - 1
+    var fabricSquaresToCut = Map[List[Int], String]()
+    for (rowNumber <- firstRow to lastRow) {
+      fabricSquaresToCut = fabricSquaresToCut ++ createRow(firstColumn, lastColumn, rowNumber)
+    }
+    fabricSquaresToCut
+  }
+
+  def createRow(firstColumn: Int, lastColumn: Int, rowNumber : Int) :Map[List[Int], String] = {
+    (firstColumn to lastColumn).map{ e => List(e, rowNumber) -> "#" }.toMap
+  }
 }
--- day03/overlap/src/test/scala/OverlapTest.scala
+++ day03/overlap/src/test/scala/OverlapTest.scala
@@ -6,4 +6,25 @@ class OverlapTest extends org.scalatest.FunSuite {
   test("Parse claim gets claim id, margin and size") {
     assert(Overlap.parseClaim("#1 @ 1,3: 4x4") === Map("claimId" -> List(1), "margins" -> List(1, 3), "size" -> List(4, 4)))
   }
+
+  test("It figures out which pieces of fabric will be cut") {
+    assert(Overlap.cutFabric("#1 @ 1,3: 4x4") === Map(
+      List(2,4) -> "#",
+      List(2,5) -> "#",
+      List(2,6) -> "#",
+      List(2,7) -> "#",
+      List(3,4) -> "#",
+      List(3,5) -> "#",
+      List(3,6) -> "#",
+      List(3,7) -> "#",
+      List(4,4) -> "#",
+      List(4,5) -> "#",
+      List(4,6) -> "#",
+      List(4,7) -> "#",
+      List(5,4) -> "#",
+      List(5,5) -> "#",
+      List(5,6) -> "#",
+      List(5,7) -> "#"
+    ))
+  }
 }
