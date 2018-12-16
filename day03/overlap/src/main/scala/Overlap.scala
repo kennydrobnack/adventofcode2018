599bb06e245543ba61d12011e5b4a21dceeaced5
Kenny Drobnack <kennydrobnack@gmail.com>
2018-12-15 23:27:19 -0500

    Saving my progress on Scala stuff. Going to take a crack at Elixir

--- day03/overlap/src/main/scala/Overlap.scala
+++ day03/overlap/src/main/scala/Overlap.scala
@@ -7,6 +7,9 @@ object Overlap extends App {
   println("Running...")
   val numberOfOverlappingCuts = countOverlaps(claims)
   println("Total number of overlapping cuts: " + numberOfOverlappingCuts)
+//  println("Calculating which cuts have no overlaps... ")
+//  val noOverlaps = findNoOverLap(claims)
+//  println("The following claim has no overlaps: " + noOverlaps)
 
   def readFromFile(fileName: String): List[String] = {
     Source.fromFile(fileName).getLines.toList
@@ -20,19 +23,20 @@ object Overlap extends App {
 
   def cutFabric(claimString: String): Map[List[Int], String] = {
     val claim = parseClaim(claimString)
+    val claimNumber = claim("claimId").head.toString
     val firstRow = claim("margins").last + 1
     val lastRow = claim("size").last + firstRow - 1
     val firstColumn = claim("margins").head + 1
     val lastColumn = firstColumn + claim("size").head - 1
     var fabricSquaresToCut = Map[List[Int], String]()
     for (rowNumber <- firstRow to lastRow) {
-      fabricSquaresToCut = fabricSquaresToCut ++ createRow(firstColumn, lastColumn, rowNumber)
+      fabricSquaresToCut = fabricSquaresToCut ++ createRow(firstColumn, lastColumn, rowNumber, claimNumber)
     }
     fabricSquaresToCut
   }
 
-  def createRow(firstColumn: Int, lastColumn: Int, rowNumber : Int) :Map[List[Int], String] = {
-    (firstColumn to lastColumn).map{ e => List(e, rowNumber) -> "#" }.toMap
+  def createRow(firstColumn: Int, lastColumn: Int, rowNumber : Int, claimNumber: String) :Map[List[Int], String] = {
+    (firstColumn to lastColumn).map{ e => List(e, rowNumber) -> claimNumber }.toMap
   }
 
   def countOverlaps(claims: List[String]) : Int = {
@@ -43,4 +47,15 @@ object Overlap extends App {
     }
     allCuts.groupBy(identity).mapValues(_.size).filter{e => e._2 > 1}.size
   }
+
+  def findNoOverLap(cuts : Map[List[Int], String], claimToCheck: Map[List[Int], String]) : Boolean = {
+    val fabricCuts = claims.map( cut => cutFabric(cut))
+    for (c <- fabricCuts) {
+      if (c.keys.intersect(claimToCheck.keys).size > 0) {
+        return false
+      }
+    }
+    true
+  }
+
 }
--- day03/overlap/src/test/scala/OverlapTest.scala
+++ day03/overlap/src/test/scala/OverlapTest.scala
@@ -9,26 +9,30 @@ class OverlapTest extends org.scalatest.FunSuite {
 
   test("It figures out which pieces of fabric will be cut") {
     assert(Overlap.cutFabric("#1 @ 1,3: 4x4") === Map(
-      List(2,4) -> "#",
-      List(2,5) -> "#",
-      List(2,6) -> "#",
-      List(2,7) -> "#",
-      List(3,4) -> "#",
-      List(3,5) -> "#",
-      List(3,6) -> "#",
-      List(3,7) -> "#",
-      List(4,4) -> "#",
-      List(4,5) -> "#",
-      List(4,6) -> "#",
-      List(4,7) -> "#",
-      List(5,4) -> "#",
-      List(5,5) -> "#",
-      List(5,6) -> "#",
-      List(5,7) -> "#"
+      List(2,4) -> "1",
+      List(2,5) -> "1",
+      List(2,6) -> "1",
+      List(2,7) -> "1",
+      List(3,4) -> "1",
+      List(3,5) -> "1",
+      List(3,6) -> "1",
+      List(3,7) -> "1",
+      List(4,4) -> "1",
+      List(4,5) -> "1",
+      List(4,6) -> "1",
+      List(4,7) -> "1",
+      List(5,4) -> "1",
+      List(5,5) -> "1",
+      List(5,6) -> "1",
+      List(5,7) -> "1"
     ))
   }
 
   test("It counts how many cuts overlap") {
     assert(Overlap.countOverlaps(List("#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2")) === 4)
+  }
+
+  test("It figures which claim has zero overlaps") {
+    assert(Overlap.findNoOverLap(List("#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2")) === 3)
   }
 }
