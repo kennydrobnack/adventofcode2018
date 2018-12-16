9fb3654fb3962d06025dd5fc5e225d604dc26ced
Kenny Drobnack <kennydrobnack@gmail.com>
2018-12-16 15:46:44 -0500

    Switched to stream processing and Enum and Map to do day 01 part 1 watching Jose Valim

--- elixir/day01.exs
+++ elixir/day01.exs
@@ -1,18 +1,11 @@
 defmodule Day01 do
-  def final_frequency(input) do
-    input
-    |> String.split("\n", trim: true)
-    |> sum_lines(0)
-  end
-
-  defp sum_lines([line | lines], current_frequency) do
-  #TODO: why is this trim needed? We're getting " +1" instead of "+1" as expected with the previous trim: true
-    new_frequency = String.to_integer(String.trim(line)) + current_frequency
-    sum_lines(lines, new_frequency)
-  end
-
-  defp sum_lines([], current_frequency) do
-    current_frequency
+  def final_frequency(file_stream) do
+    file_stream
+    |> Stream.map(fn line -> 
+        {integer, _leftover} = Integer.parse(line)
+        integer
+      end)
+    |> Enum.sum()
   end
 end
 
@@ -27,17 +20,18 @@ case System.argv() do
     import Day01
 
     test "final frequency" do
-      assert final_frequency("""
-        +1
-        +1
-        +1
-      """) == 3
+      {:ok, io} = StringIO.open("""
++1
++1
++1
+""")
+      assert final_frequency(IO.stream(io, :line)) == 3
     end
   end
 
 [input_file] -> 
   input_file
-  |> File.read!()
+  |> File.stream!([], :line)
   |> Day01.final_frequency()
   |> IO.puts
 
